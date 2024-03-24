583c93fe5f9dca39ae59b78b63584723cd796cd3
Marcelo Basile <basile_macelo@yahoo.com.ar>
2024-03-24 10:27:56 -0300

    Proyecto ok 24-3-24/2

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
 
 
 const Form = () => {
   //Aqui deberan implementar el form completo con sus validaciones
 
+  const [user, setUser] = useState({
+    name: '',
+    email: ''
+  })
+
+  const [show, setShow] = useState(false)
+  const [err, setErr] = useState(false)
+
+  const handlerSubmit = (event) => {
+    event.preventDefault()
+    const eRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
+    if(user.name.length > 5 && eRegex.test(user.email)){
+      setShow(true)
+      setErr(false)
+    } else {
+        setErr(true)
+      }
+    }
+
   return (
     <div>
-      <form>
+      {!show ? 
+      <form onSubmit={handlerSubmit}>
+          <label>Nombre Completo:</label>
+          <input type="text" onBlur={(event) => setUser({...user, name: event.target.value})}/>
+          <label>Email:</label>
+          <input type="text" onBlur={(event) => setUser({...user, email: event.target.value})}/>
+          <br />
+          <button>Enviar</button>
       </form>
+      : `${user.name} sus datos fuenon enviados ok...`}
+      {err ? 'Verifique los datos' : null}
     </div>
   );
 };
--- src/Routes/Contact.jsx
+++ src/Routes/Contact.jsx
@@ -6,8 +6,8 @@ import Form from '../Components/Form'
 const Contact = () => {
   return (
     <div>
-      <h2>Want to know more?</h2>
-      <p>Send us your questions and we will contact you</p>
+      <h2>En que podemos ayudarte?</h2>
+      <p>Envianos tu consulta y te contactaremos a la brevedad...</p>
       <Form/>
     </div>
   )
